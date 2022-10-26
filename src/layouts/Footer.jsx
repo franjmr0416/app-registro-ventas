90e667d0338c495c988f632b37986274fdcb690a
franjmr0416 <franjmr16@gmail.com>
2022-10-26 15:16:00 -0500

    fix: footer text size

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -10,19 +10,19 @@ const Footer = () => {
     <div className='flex justify-evenly bg-white py-2 sticky inset-x-0 bottom-0 visible lg:invisible'>
       <div className=''>
         <FaCashRegister className='text-3xl mx-auto text-indigo-400' />
-        <p>Ventas</p>
+        <p className='text-xs'>Ventas</p>
       </div>
       <div>
         <FaChartLine className='text-3xl mx-auto' />
-        <p>Gráficos</p>
+        <p className='text-xs'>Gráficos</p>
       </div>
       <div>
         <FaBars className='text-3xl mx-auto' />
-        <p>Opciones</p>
+        <p className='text-xs'>Opciones</p>
       </div>
       <div>
         <FaPlusCircle className='text-3xl mx-auto' />
-        <p>Agregar</p>
+        <p className='text-xs'>Agregar</p>
       </div>
     </div>
   )
--- src/layouts/List.jsx
+++ src/layouts/List.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from 'react'
 import Card from '../components/Card'
+import { supabase } from '../services/supabaseClient'
+
+const getVentas = async () => {
+  let { data: Ventas, error } = await supabase.from('Ventas').select('*')
+
+  console.log('data: ' + Ventas)
+  console.log('error: ' + error)
+}
 
 const List = () => {
+  const [ventas, setVentas] = useState(null)
+  getVentas()
+  /*
+  useEffect(() => {
+    setVentas(getVentas())
+  },[ventas])
+  */
   return (
     <div className='flex flex-col pt-1'>
       <Card />
