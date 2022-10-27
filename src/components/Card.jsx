df519f1e419e6b984c5ca5482233eecb560362d0
franjmr0416 <franjmr16@gmail.com>
2022-10-27 17:18:24 -0500

    fix: complete card data #2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,23 @@
 import { FaUserAlt } from 'react-icons/fa'
 
-const Card = () => {
+const Card = (props) => {
+  const { fecha_creacion, total, status, nombre, foto } = props
   return (
     <div className='flex flex-col mx-2 my-1 p-2 rounded-md shadow-md bg-white'>
       <div className='flex justify-between pb-2'>
         <div className='flex justify-between'>
           <FaUserAlt className='text-3xl' />
-          <p className='pl-3'>Nombre Cliente</p>
+          <p className='pl-3'>{nombre}</p>
         </div>
         <div>
           <span className='bg-indigo-100 text-indigo-800 text-sm font-semibold px-2.5 py-0.5 rounded'>
-            Status
+            {status}
           </span>
         </div>
       </div>
       <div className='flex justify-between border-t-2 pt-1'>
-        <p className='text-gray-500'>26-10-2022</p>
-        <span className='text-lg font-semibold'>$250.00</span>
+        <p className='text-gray-500'>{fecha_creacion}</p>
+        <span className='text-lg font-semibold'>${total}</span>
       </div>
     </div>
   )
--- src/layouts/List.jsx
+++ src/layouts/List.jsx
@@ -2,31 +2,35 @@ import { useEffect, useState } from 'react'
 import Card from '../components/Card'
 import { supabase } from '../services/supabaseClient'
 
-const getVentas = async () => {
-  let { data: Ventas, error } = await supabase.from('Ventas').select('*')
-
-  console.log('data: ' + Ventas)
-  console.log('error: ' + error)
-}
-
 const List = () => {
-  const [ventas, setVentas] = useState(null)
-  getVentas()
-  /*
+  const [ventas, setVentas] = useState([])
+
+  const getVentas = async () => {
+    let { data, error } = await supabase
+      .from('Ventas')
+      .select(`id, fecha_creacion, total, status, Clientes(nombre, avatar_url)`)
+
+    setVentas(data)
+  }
+
   useEffect(() => {
-    setVentas(getVentas())
-  },[ventas])
-  */
+    getVentas()
+  }, [ventas])
+
   return (
     <div className='flex flex-col pt-1'>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {ventas.map((item) => {
+        return (
+          <Card
+            key={item.id}
+            fecha_creacion={item.fecha_creacion}
+            total={item.total}
+            status={item.status}
+            nombre={item.Clientes.nombre}
+            foto={item.Clientes.avatar_url}
+          />
+        )
+      })}
     </div>
   )
 }
