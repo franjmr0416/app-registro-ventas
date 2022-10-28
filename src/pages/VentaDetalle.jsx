b18c5faf44987ef33fee83e7eef8f5f45afb30c9
franjmr0416 <franjmr16@gmail.com>
2022-10-28 13:26:23 -0500

    add: get venta request

--- src/pages/VentaDetalle.jsx
+++ src/pages/VentaDetalle.jsx
@@ -1,13 +1,34 @@
 import PageContainer from '../layouts/PageContainer'
 import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { supabase } from '../services/supabaseClient'
 
 const VentaDetalle = () => {
   const { id } = useParams()
+  const [venta, setVenta] = useState({})
+
+  const getVenta = async () => {
+    let { data, error } = await supabase
+      .from('Ventas')
+      .select(
+        `id, fecha_creacion, total, status, Clientes(nombre), Ventas-Productos(cantidad), Productos(nombre, descripcion, precio_venta)`
+      )
+      .eq('id', id)
+
+    setVenta(data[0])
+    console.log(venta)
+  }
+
+  useEffect(() => {
+    getVenta()
+  }, [venta])
 
   return (
-    <PageContainer>
-      <div>Venta Detalle id= {id}</div>
-    </PageContainer>
+    <div className='flex flex-col p-4'>
+      <p>#Pedido: {venta.id}</p>
+      <span>{venta.status}</span>
+      <p>Fecha: {venta.fecha_creacion}</p>
+    </div>
   )
 }
 export default VentaDetalle
