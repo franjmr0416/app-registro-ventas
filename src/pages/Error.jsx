7e01b67f3998be0ab5343c04ec3b808fc4a3bd8b
franjmr0416 <franjmr16@gmail.com>
2022-10-27 23:23:18 -0500

    add: error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -2,9 +2,15 @@ import PageContainer from '../layouts/PageContainer'
 
 const ListPage = () => {
   return (
-    <PageContainer>
-      <div>Error 404</div>
-    </PageContainer>
+    <div className='flex flex-col p-4 justify-center items-center'>
+      <p className='text-2xl font-semibold text-indigo-700'>Error 404</p>
+      <p className='text-lg'>Lo que sea que estes buscando no esta aquí</p>
+      <img
+        className='mt-10 max-w-lg'
+        alt='error 404'
+        src='../../public/error404.svg'
+      />
+    </div>
   )
 }
 export default ListPage
