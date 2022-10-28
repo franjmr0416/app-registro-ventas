e4505b354fccf7e947b8bf47a0837493d8f714dc
franjmr0416 <franjmr16@gmail.com>
2022-10-27 22:36:09 -0500

    add: navigation to venta detalle

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ListPage from './pages/ListPage'
+import VentaDetalle from './pages/VentaDetalle'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<ListPage />} />
+        <Route path='ventadetalle' element={<VentaDetalle />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/VentaDetalle.jsx
@@ -0,0 +1,15 @@
+import PageContainer from '../layouts/PageContainer'
+import Nav from '../layouts/Nav'
+
+import Footer from '../layouts/Footer'
+
+const ListPage = () => {
+  return (
+    <PageContainer>
+      <Nav />
+      <div>VentaDetalle</div>
+      <Footer />
+    </PageContainer>
+  )
+}
+export default ListPage
