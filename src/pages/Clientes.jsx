eb6b358364870a530c22b16714bdb78601200e88
franjmr0416 <franjmr16@gmail.com>
2022-10-31 20:28:36 -0600

    feat: basic list clientes

--- src/pages/Clientes.jsx
+++ src/pages/Clientes.jsx
@@ -1,10 +1,36 @@
+import { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
+import { supabase } from '../services/supabaseClient'
+import { Link } from 'react-router-dom'
+import { FaIdCard } from 'react-icons/fa'
 
 const Clientes = () => {
+  const [clientes, setClientes] = useState([])
+
+  const getClientes = async () => {
+    let { data, error } = await supabase.from('Clientes').select()
+    setClientes(data)
+  }
+
+  useEffect(() => {
+    getClientes()
+  }, [clientes])
+
   return (
     <div className='flex flex-col'>
       <Navbar title='Opciones' routeAdd='options' />
-      <p>listado clientes</p>
+      <div className='flex flex-col p-4 bg-gray-200 h-screen'>
+        {clientes.map((x) => {
+          return (
+            <Link to={`clientes/${x.id}`}>
+              <div className='flex items-center shadow rounded-lg p-4 mb-2 bg-white'>
+                <FaIdCard className='text-xl text-indigo-700' />
+                <span className='ml-2'>{x.nombre}</span>
+              </div>
+            </Link>
+          )
+        })}
+      </div>
     </div>
   )
 }
--- src/services/crud.js
+++ src/services/crud.js
@@ -1,7 +1,12 @@
 import { supabase } from './supabaseClient'
 
+const readRegisters = async (table) => {
+  const { data, error } = await supabase.from(table).select()
+  return data
+}
+
 const createRegister = async (table, data) => {
   const { error } = await supabase.from(table).insert(data)
 }
 
-export { createRegister }
+export { readRegisters, createRegister }
