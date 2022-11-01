0ab95f4d497048c0e4b452bc084c6ff99e1ec088
franjmr0416 <franjmr16@gmail.com>
2022-10-31 20:54:09 -0600

    mod: dynamic back button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import { FaArrowLeft } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Navbar = (props) => {
   const { title, routeAdd } = props
+  const navigate = useNavigate()
 
   return (
     <nav className='shadow-lg p-4 flex justify-between'>
       <div className='flex items-center'>
-        <Link to={'/'}>
+        <div onClick={() => navigate(-1)}>
           <FaArrowLeft className='text-2xl text-gray-400' />
-        </Link>
+        </div>
         <span className='text-lg ml-2'>{title ? title : 'Atras'}</span>
       </div>
       {routeAdd ? (
